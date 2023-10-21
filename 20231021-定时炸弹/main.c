ea01daeff57716a76c776d68329d50bbe0e3f9c5
Fumeng24 <114860867+Fumeng24@users.noreply.github.com>
2023-10-21 10:07:21 +0800

    Update main.c
    删除无关注释

--- 20231021-定时炸弹/main.c
+++ 20231021-定时炸弹/main.c
@@ -3,35 +3,6 @@ sbit we = P2 ^ 7;
 sbit du = P2 ^ 6;
 sbit feng = P2 ^ 3;
 
-
-//unsigned char number[]=
-//	{
-//	0x3F,  //"0"
-//	0x06,  //"1"
-//	0x5B,  //"2"
-//	0x4F,  //"3"
-//	0x66,  //"4"
-//	0x6D,  //"5"
-//	0x7D,  //"6"
-//	0x07,  //"7"
-//	0x7F,  //"8"
-//	0x6F,  //"9"
-//	0x77,  //"A"
-//	0x7C,  //"B"
-//	0x39,  //"C"
-//	0x5E,  //"D"
-//	0x79,  //"E"
-//	0x71,  //"F"
-//	0x76,  //"H"
-//	0x38,  //"L"
-//	0x37,  //"n"
-//	0x3E,  //"u"
-//	0x73,  //"P"
-//	0x5C,  //"o"
-//	0x40,  //"-"
-//	0x00,  //Ï¨Ãð
-//	0x00  //×Ô¶¨Òå
-//	};
 unsigned char position[]=
 	{
 	0xff,	//1111 1111
@@ -69,10 +40,6 @@ feng=0x00;
 delay(50);
 feng = 0xff;
 delay(50);
-//P1=0x00;
-//delay(50);
-//P1 = 0xff;
-//delay(50);
 }
 
 void shumaguan(unsigned char digit) {
